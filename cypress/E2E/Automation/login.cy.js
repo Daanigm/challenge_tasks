7eaacd79b1f832714327c4a18a55546a784c5335
daanigm <daaniigm@gmail.com>
2025-03-09 19:56:42 +0100

    update

--- cypress/E2E/Automation/login.cy.js
+++ cypress/E2E/Automation/login.cy.js
@@ -1,6 +1,6 @@
-describe('Acceder a My Account', () => {
-  it('accede correctamente a la opción My Account', () => {
-    // Paso 1: Visitar la página con autenticación
+describe('Login Tests', () => {
+
+  it('Correct login', () => {
     cy.visit('https://qatesting-wand-stg.cpe.gigmagic.io/en', {
       auth: {
         username: 'qatesting',
@@ -8,27 +8,24 @@ describe('Acceder a My Account', () => {
       }
     });
 
-    // Paso 2: Aceptar el popup de cookies y asegurarse de que no vuelva a aparecer
     cy.get('#content > magic-notifications')
-      .shadow()  // Acceder al shadow DOM
+      .shadow()  
       .find('magic-ui-notification:nth-child(1)')
-      .shadow()  // Acceder al siguiente shadow DOM
+      .shadow() 
       .find('magic-cookie-popup-notification')
-      .shadow()  // Acceder al siguiente shadow DOM
-      .find('div > magic-ui-button')  // Encuentra el botón para aceptar
-      .click();  // Hacer clic en el botón para aceptar cookies
+      .shadow()  
+      .find('div > magic-ui-button')  
+      .click();  
 
- 
     cy.get('#content > magic-notifications')
       .shadow()
       .find('magic-ui-notification:nth-child(1)')
       .should('not.exist');  
 
-    
     cy.get('.buttons > [variant="primary"]', { timeout: 2000 }) 
       .should('be.visible') 
       .click(); 
-   
+
     cy.get('#email', { timeout: 1000 })  
       .should('be.visible')  
       .click() 
@@ -39,14 +36,91 @@ describe('Acceder a My Account', () => {
       .click()  
       .type('Testerc00-');  
 
-    
     cy.get('.login-form > .action > .button', { timeout: 1000 })  
       .should('be.visible')  
       .click();  
-
-
-      
-    });
   });
 
-  
+  it('Incorrect login', () => {
+    cy.visit('https://qatesting-wand-stg.cpe.gigmagic.io/en', {
+      auth: {
+        username: 'qatesting',
+        password: 'qatesting_gig'
+      }
+    });
+
+    cy.get('#content > magic-notifications')
+      .shadow()  
+      .find('magic-ui-notification:nth-child(1)')
+      .shadow() 
+      .find('magic-cookie-popup-notification')
+      .shadow()  
+      .find('div > magic-ui-button')  
+      .click();  
+
+    cy.get('#content > magic-notifications')
+      .shadow()
+      .find('magic-ui-notification:nth-child(1)')
+      .should('not.exist');  
+
+    cy.get('.buttons > [variant="primary"]', { timeout: 2000 }) 
+      .should('be.visible') 
+      .click(); 
+
+    cy.get('#email', { timeout: 1000 })  
+      .should('be.visible')  
+      .click() 
+      .type('emailfail@gig.com');
+
+    cy.get('.peek-password > .form-field > .input', { timeout: 1000 }) 
+      .should('be.visible') 
+      .click()  
+      .type('Testrc00-');  
+
+    cy.get('.login-form > .action > .button', { timeout: 1000 })  
+      .should('be.visible')  
+      .click();  
+  });
+
+  it('Login with invalid characters', () => {
+    cy.visit('https://qatesting-wand-stg.cpe.gigmagic.io/en', {
+      auth: {
+        username: 'qatesting',
+        password: 'qatesting_gig'
+      }
+    });
+
+    cy.get('#content > magic-notifications')
+      .shadow()  
+      .find('magic-ui-notification:nth-child(1)')
+      .shadow() 
+      .find('magic-cookie-popup-notification')
+      .shadow()  
+      .find('div > magic-ui-button')  
+      .click();  
+
+    cy.get('#content > magic-notifications')
+      .shadow()
+      .find('magic-ui-notification:nth-child(1)')
+      .should('not.exist');  
+
+    cy.get('.buttons > [variant="primary"]', { timeout: 2000 }) 
+      .should('be.visible') 
+      .click(); 
+
+    cy.get('#email', { timeout: 1000 })  
+      .should('be.visible')  
+      .click() 
+      .type('---2ñ@gig.com');
+
+    cy.get('.peek-password > .form-field > .input', { timeout: 1000 }) 
+      .should('be.visible') 
+      .click()  
+      .type('Testerc00-');  
+
+    cy.get('.login-form > .action > .button', { timeout: 1000 })  
+      .should('be.visible')  
+      .click();  
+  });
+
+});
